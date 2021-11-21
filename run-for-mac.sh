feed05362f649165d2bbf8898437a480fe3ec066
sunyuantao <sunyuantao@galaxy-future.com>
2021-11-21 23:39:00 +0800

    deploy for mac

--- run-for-mac.sh
+++ run-for-mac.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # deploy fe for mac env
-sed "s/127.0.0.1/host.docker.internal/g" $(pwd)/server/config/host.js > $(pwd)/server/config/host.js.mac
+sed "s/127.0.0.1:9090/host.docker.internal:9099/g" $(pwd)/server/config/host.js > $(pwd)/server/config/host.js.mac
 docker run -d --name bridgx_fe -p 80:80 --add-host host.docker.internal:host-gateway -v $(pwd)/server/config/host.js.mac:/bridgx-fe/server/config/host.js galaxyfuture/bridgx-fe:v0.1.0
