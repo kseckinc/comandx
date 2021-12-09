d0101a56cbdf735f6e76c218fc2bfc6454f22198
yangyulong@galaxy-future.com <Yangyulong908>
2021-12-09 13:47:37 +0800

    fix

--- server/config/host.js
+++ server/config/host.js
@@ -1,13 +1,13 @@
-// const bridgx_host = 'http://127.0.0.1:9090'
+const bridgx_host = 'http://127.0.0.1:9090'
 // const bridgx_host = 'http://bridgx-api.internal.galaxy-future.org'
-const bridgx_host = 'http://bridgx-api-test.internal.galaxy-future.org'
+// const bridgx_host = 'http://bridgx-api-test.internal.galaxy-future.org'
 
-const schedulx_host = 'http://10.16.23.96:9090'
-// const schedulx_host = 'http://127.0.0.1:9091'
+// const schedulx_host = 'http://10.16.23.96:9090'
+const schedulx_host = 'http://127.0.0.1:9091'
 
-// const kube_host = 'http://127.0.0.1:8080'
+const kube_host = 'http://127.0.0.1:9090'
 // const kube_host = 'http://bridgx-containers.internal.galaxy-future.org'
-const kube_host = 'http://bridgx-api-test.internal.galaxy-future.org'
+// kube_host = 'http://bridgx-api-test.internal.galaxy-future.org'
 
 function getBridgXHost() {
   return bridgx_host
