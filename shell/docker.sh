4a145469b984e8adb692df0e30078605cdccf3eb
yangyulong@galaxy-future.com <Yangyulong908>
2021-12-13 14:22:41 +0800

    fix title

--- shell/docker.sh
+++ shell/docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cd ../server && docker build -t 127.0.0.1:12380/bridgx/bridgx-fe-2:0.0.1 . && docker push 127.0.0.1:12380/bridgx/bridgx-fe-2:0.0.1
+cd ../server && docker build -t 127.0.0.1:12380/comandx/comandx:0.2.0 . && docker push 127.0.0.1:12380/comandx/comandx:0.2.0
