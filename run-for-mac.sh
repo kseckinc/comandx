2f593c67487380f6d45461f2a437d120fcee79d8
yulong yang <94545053+GFYulongYang@users.noreply.github.com>
2021-12-22 16:11:59 +0800

    Update run-for-mac.sh

--- run-for-mac.sh
+++ run-for-mac.sh
@@ -2,4 +2,4 @@
 # deploy fe for mac env
 sed "s/127.0.0.1:9090/host.docker.internal:9099/g" $(pwd)/server/config/host.js > $(pwd)/server/config/host.js.1
 sed "s/127.0.0.1:9091/host.docker.internal:9091/g" $(pwd)/server/config/host.js.1 > $(pwd)/server/config/host.js.mac
-docker run -d --name comandx -p 80:80 --add-host host.docker.internal:host-gateway -v $(pwd)/server/config/host.js.mac:/bridgx-fe/server/config/host.js galaxyfuture/comandx:latest
+docker run -d --name comandx -p 80:80 --add-host host.docker.internal:host-gateway -v $(pwd)/server/config/host.js.mac:/comandx/server/config/host.js galaxyfuture/comandx:latest
